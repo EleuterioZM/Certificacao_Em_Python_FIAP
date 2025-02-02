f852698ae76bcdded808042d3978fd120a4140ad
Eleuterio Zacarias Mabecuane <122021325+EleuterioZM@users.noreply.github.com>
2025-02-02 12:25:26 +0200

    #Feature: Estrutura de decisao

---
+++ decisao_if.py
@@ -0,0 +1,7 @@
+name = input("Digite seu nome :")
+idade = int(input("Digite sua idade :"))
+
+if idade >= 65 :
+    print(f"O paciente {name}, POSSUI um atendimento prioritario")
+else :
+    print(f"O paciente {name}, NAO possui o  atendimento prioritario")
