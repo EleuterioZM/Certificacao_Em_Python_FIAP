39d1a1a7f04040428d9715fcfe0fabdb04d53ad7
Eleuterio Zacarias Mabecuane <122021325+EleuterioZM@users.noreply.github.com>
2025-02-02 13:13:10 +0200

    #Feature: Estruturas de decisao

--- decisao_if.py
+++ decisao_if.py
@@ -1,7 +1,9 @@
 name = input("Digite seu nome :")
 idade = int(input("Digite sua idade :"))
-
+doencaInfectocontagiosa = input("Suspeita de uma doenca infectocontagiosa? ").upper()
 if idade >= 65 :
     print(f"O paciente {name}, POSSUI um atendimento prioritario")
+elif  doencaInfectocontagiosa == "SIM" :
+    print(f"O paciente {name} deve ser direcionado a sala de reserva")
 else :
     print(f"O paciente {name}, NAO possui o  atendimento prioritario")
---
+++ decisao_while.py
@@ -0,0 +1,5 @@
+numero = int(input("Digite o numero :" ))
+while numero < 100 :
+    print("\t" + str(numero))
+    numero+=1
+print("Laco encerrado")
