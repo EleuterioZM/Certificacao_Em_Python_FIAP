a072552e59802ca512ba62ca55bd8f2b7b0f0169
Eleuterio Zacarias Mabecuane <122021325+EleuterioZM@users.noreply.github.com>
2025-02-02 12:11:18 +0200

    #First commit

---
+++ variaveis.py
@@ -0,0 +1,9 @@
+name = input("Digite seu nome :")
+sabreName = input("Digite seu sobrenome :")
+print("ola," +"" + name +"_"+ sabreName +", "+ "seja bem vido(a))")
+anoDeNascimento = int(input("Digite seu ano de nascimento: "))
+anoActual = int(input("Em que ano estamos? "))
+
+suaIdade = anoActual - anoDeNascimento
+print(f"Então, você tem {suaIdade}  anos de idade!")
+
